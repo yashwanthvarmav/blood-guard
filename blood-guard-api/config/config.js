a1df46abf24d86f6af14c63f8f71b9b72507df50
Yashwanth Varma V <157632442+yashwanthvarmav@users.noreply.github.com>
2024-12-31 18:22:42 +0530

    Update config.js

--- blood-guard-api/config/config.js
+++ blood-guard-api/config/config.js
@@ -7,12 +7,12 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Allow self-signed certificates
-      },
-    }
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false, // Allow self-signed certificates
+    //   },
+    // }
   },
   test: {
     username: process.env.DB_USER,
@@ -20,24 +20,24 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Allow self-signed certificates
-      },
-    }
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false, // Allow self-signed certificates
+    //   },
+    // }
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "postgres"
-  },
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Allow self-signed certificates
-    },
+    dialect: "postgres",
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false, // Allow self-signed certificates
+    //   },
+    // },
   },
 }
